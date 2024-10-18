3c0664435a66233868c9b085517b8ea8c3cc7c2f
Kaspar Bergert <50704267035@tptlive.ee>
2024-10-18 10:22:49 +0300

    tunnitöö 1

--- KooliProjekt/Data/ApplicationDbContext.cs
+++ KooliProjekt/Data/ApplicationDbContext.cs
@@ -9,5 +9,20 @@ namespace KooliProjekt.Data
             : base(options)
         {
         }
+
+        // DbSets for your application models
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<CompletedTask> CompletedTasks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<LogCollection> LogCollections { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectDetails> ProjectDetails { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskDetails> TaskDetails { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cleaner> Cleaners { get; set; }
+
     }
 }
