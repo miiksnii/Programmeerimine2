6a9cb9f4a410b70e6e73304f0e28d77ec918970d
Kaspar Bergert <50704267035@tptlive.ee>
2024-12-06 10:41:39 +0200

    push 3

---
+++ WebApplication1/Controllers/EmployeeListsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Properties.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeListsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeListsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EmployeeLists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.EmployeeLists.ToListAsync());
+        }
+
+        // GET: EmployeeLists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeList = await _context.EmployeeLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeList == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeList);
+        }
+
+        // GET: EmployeeLists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeLists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] EmployeeList employeeList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employeeList);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeList);
+        }
+
+        // GET: EmployeeLists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeList = await _context.EmployeeLists.FindAsync(id);
+            if (employeeList == null)
+            {
+                return NotFound();
+            }
+            return View(employeeList);
+        }
+
+        // POST: EmployeeLists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] EmployeeList employeeList)
+        {
+            if (id != employeeList.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employeeList);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeListExists(employeeList.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeList);
+        }
+
+        // GET: EmployeeLists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeList = await _context.EmployeeLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeList == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeList);
+        }
+
+        // POST: EmployeeLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employeeList = await _context.EmployeeLists.FindAsync(id);
+            if (employeeList != null)
+            {
+                _context.EmployeeLists.Remove(employeeList);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeListExists(int id)
+        {
+            return _context.EmployeeLists.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProjectListsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Properties.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class ProjectListsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectListsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProjectLists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProjectLists.ToListAsync());
+        }
+
+        // GET: ProjectLists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var projectList = await _context.ProjectLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (projectList == null)
+            {
+                return NotFound();
+            }
+
+            return View(projectList);
+        }
+
+        // GET: ProjectLists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectLists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] ProjectList projectList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(projectList);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(projectList);
+        }
+
+        // GET: ProjectLists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var projectList = await _context.ProjectLists.FindAsync(id);
+            if (projectList == null)
+            {
+                return NotFound();
+            }
+            return View(projectList);
+        }
+
+        // POST: ProjectLists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] ProjectList projectList)
+        {
+            if (id != projectList.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(projectList);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectListExists(projectList.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(projectList);
+        }
+
+        // GET: ProjectLists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var projectList = await _context.ProjectLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (projectList == null)
+            {
+                return NotFound();
+            }
+
+            return View(projectList);
+        }
+
+        // POST: ProjectLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var projectList = await _context.ProjectLists.FindAsync(id);
+            if (projectList != null)
+            {
+                _context.ProjectLists.Remove(projectList);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProjectListExists(int id)
+        {
+            return _context.ProjectLists.Any(e => e.Id == id);
+        }
+    }
+}
